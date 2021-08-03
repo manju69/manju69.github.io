3af0effd6078461ee692be7290352db97201f14d
manju69 <87670779+manju69@users.noreply.github.com>
2021-08-03 10:29:57 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 
 let account = new Account("12345");
-let savingAccount = new SavingAccount("2345",5);
-savingAccount.addInterest();
+let savingAccount = new SavingAccount("62471",5);
+let checkingAccount = new CheckingAccount("8191535", 300);
+let bank = new Bank();
 account.deposit(2334);
-
 
 describe("getNumber", function () {
     it("returns the account number",
@@ -37,7 +37,23 @@ describe("endOfMonth", function () {
         });
 });
 
+savingAccount.deposit(90000);
+savingAccount.setInterest(10);
+savingAccount.addInterest();
 
+describe ("savingsgetInterest", function(){
+    it("returns the interest of the savings Account", function(){
+        assert.equal(savingAccount.getInterest(),"10");
+    }
+    )
+});
+
+describe ("savingsgetBalance", function(){
+    it("returns the amount on the savings account plus interest", function(){
+        assert.equal(savingAccount.getBalance(),99000);
+    }
+    )
+});
 
 describe("toString", function () {
     it("return string that contain balance and account number information",
